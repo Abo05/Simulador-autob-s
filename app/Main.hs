dbdf2282da0e3436a38ecae756c796c7584c365b
Abo05 <alvaro.acedoblanco05@gmail.com>
2026-07-01 18:51:12 +0200

    Puesto mensaje de ayuda

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,36 @@
 module Main (main) where
 
 import System.Environment (getArgs)
+import System.Exit (exitSuccess)
 import Control.Exception (finally)
 import Parser (getAppConfig)
 import Motor (runSimulation)
 
+helpMessage :: String
+helpMessage = unlines
+    [ "Uso: Autobus [OPCIONES]"
+    , ""
+    , "Simulador estocástico de una parada de autobús mediante eventos discretos."
+    , ""
+    , "Opciones disponibles:"
+    , "  -h, --help    Muestra este mensaje de ayuda y termina."
+    , "  -s  <float>   Media de personas por grupo (Poisson). Por defecto: 1.5"
+    , "  -t  <float>   Tasa de llegada de grupos (Exponencial). Por defecto: 0.5"
+    , "  -f  <float>   Tiempo base entre autobuses. Por defecto: 5.5"
+    , "  -c  <int>     Capacidad máxima del autobús. Por defecto: 30"
+    , "  -a  <float>   Parámetro alfa (Beta) de llegada anticipada/coordinada. Por defecto: 4.0"
+    , "  -pp <float>   Sensibilidad de abandono por la multitud en la parada. Por defecto: 0.05"
+    , "  -pt <float>   Sensibilidad de abandono por el tiempo de espera. Por defecto: 0.02"
+    ]
+
 main :: IO ()
 main = do
     args <- getArgs
-    let conf = getAppConfig args  -- 1. Conseguir configuración limpia
-    runSimulation conf `finally` putStr "\x1b[0m"
+    
+    case args of
+        (arg:_) | arg `elem` ["-h", "--help"] -> do
+            putStr helpMessage
+            exitSuccess
+        _ -> do
+            let conf = getAppConfig args  
+            runSimulation conf `finally` putStr "\x1b[0m"
--- src/Motor.hs
+++ src/Motor.hs
@@ -6,10 +6,11 @@ import Bus (nextBus, Bus(..))
 import Events (EventType(..), Event, pushEvent, Time)
 import Parser (AppConfig(..))
 
-cGreen, cYellow, cRed:: String
+cGreen, cYellow, cRed, cReset :: String
 cGreen  = "\x1b[32m"
 cYellow = "\x1b[33m"
 cRed    = "\x1b[31m"
+cReset  = "\x1b[0m"
 
 pushPassengers :: Time -> ([Time], [WaitingPassengers]) -> [Event] -> [Event]
 pushPassengers _ ([], _) e = e
@@ -35,7 +36,7 @@ loop conf tNextBus waiting pendingBus ((eventTime, eventType) : restQueue) = do
     case eventType of
         GroupArrival amount -> do
             let totalWaiting = waiting + amount
-            putStrLn $ cYellow ++ "[" ++ show eventTime ++ "] LLEGA GRUPO. Tamaño: " ++ show amount ++ ". Esperando en parada: " ++ show totalWaiting
+            putStrLn $ cYellow ++ "[" ++ show eventTime ++ "] LLEGA GRUPO. Tamaño: " ++ show amount ++ ". Esperando en parada: " ++ show totalWaiting ++ cReset
             
             let timeNextBus = max 0 (tNextBus - eventTime)
             let l1 = patPass conf
@@ -60,7 +61,7 @@ loop conf tNextBus waiting pendingBus ((eventTime, eventType) : restQueue) = do
             let leftBehind = busInStop arrivingBus waiting
             let boarded = waiting - leftBehind
             
-            putStrLn $ cGreen ++ "[" ++ show eventTime ++ "] LLEGA BUS. Han subido: " ++ show boarded ++ " pasajeros. Se quedan: " ++ show leftBehind
+            putStrLn $ cGreen ++ "[" ++ show eventTime ++ "] LLEGA BUS. Han subido: " ++ show boarded ++ " pasajeros. Se quedan: " ++ show leftBehind ++ cReset
             
             let fr = freq conf
             let cp = cap conf
@@ -82,7 +83,7 @@ loop conf tNextBus waiting pendingBus ((eventTime, eventType) : restQueue) = do
         GroupAbandonment amount -> do
 
             let totalWaiting = max 0 (waiting - amount)
-            putStrLn $ cRed ++ "[" ++ show eventTime ++ "] SE VA GRUPO. Tamaño: " ++ show amount ++ ". Esperando en parada: " ++ show totalWaiting
+            putStrLn $ cRed ++ "[" ++ show eventTime ++ "] SE VA GRUPO. Tamaño: " ++ show amount ++ ". Esperando en parada: " ++ show totalWaiting ++ cReset
 
             threadDelay 250000 
             loop conf tNextBus totalWaiting pendingBus restQueue
